6c4dca4019d158d840eca221550f00f27d9680a7
Vidit Shah <vidit0210@gmail.com>
2018-11-13 15:12:41 +0530

    Vidly Express Phase1 : Complete

--- VidlyCdStore.js
+++ VidlyCdStore.js
@@ -10,7 +10,7 @@ const data = [
 
 function verify(request){
     const schema = {genre:Joi.string().required(),
-                    code:Joi.string.required()};
+                    code:Joi.string().required()};
      let result = Joi.validate(request,schema);
     return result;
 }
@@ -18,5 +18,41 @@ app.get('/api/genre',(req,res)=>{
     res.send('Welcome to Genre');
     console.log('Got Simple Get Request!');
 })
-app.post()
+app.post('/api/genre',(req,res)=>{
+    console.log(data);
+    const {error} =verify(req.body);
+    if(error){return res.status(200).send(error.details.message[0]) };
+    const add ={
+        id:data.length+1,
+        genre:req.body.genre,
+        code:req.body.code
+    };
+    data.push(add);
+    res.send(data);
+    console.log("Added Data is",data);
+});
+
+app.put('/api/genre/:id',(req,res)=>{
+    const result = verify(req.body);
+    if(result.error){ return res.status(400).send(error.details.message[0])};
+    let search = data.find(s=>s.id==req.params.id);
+    if(!search){res.send('Item Not Found')}
+    search.genre = req.body.genre;
+    search.code= req.body.code;
+    res.send(data);
+    console.log("Put Command Worked Well")
+})
+
+app.delete('/api/genre/:id',(req,res)=>{
+    //search for the Element if it Exits or not
+    let search = data.find(s=>s.id==req.params.id);
+    if(!search){
+        return res.send('Item not found');
+    }
+    let index = data.findIndex(search);
+    data.splice(index,1);
+    res.send('Data Deleted');
+    console.log(data);
+})
+
 app.listen(3000,()=>console.log('Listening to Port 3000'));
